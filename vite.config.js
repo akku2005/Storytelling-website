fe7713d02f3bebe3af07aed051b2ae83d6d9714a
akku2005 <asakashsahu20@gmail.com>
2024-11-10 14:36:51 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: {
+      host: "localhost", // You can adjust this to your environment
+      protocol: "ws", // Or 'wss' for secure WebSocket
+    },
+  },
   define: {
-    __DEFINES__: JSON.stringify({}), // Add any key-value pairs you need here
+    __DEFINES__: JSON.stringify({}),
   },
 });
